5402004b9d250d8e68118b4097401e9cb11eb002
morax25 <adarsh@infoneotech.com>
2026-05-07 01:41:24 +0530

    updated icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ArrowBigRight } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 const page = () => {
@@ -53,11 +54,11 @@ const page = () => {
           onClick={() => {
             router.push("/app");
           }}
-          className="w-full cursor-pointer py-4 rounded-full bg-[#0a0a0f] border border-purple-500/40 text-white font-bold text-sm tracking-[0.2em] uppercase
+          className="w-full flex justify-center items-center gap-2 cursor-pointer py-4 rounded-full bg-[#0a0a0f] border border-purple-500/40 text-white font-bold text-sm tracking-[0.2em] uppercase
             shadow-[0_0_10px_rgba(168,85,247,0.4),0_0_20px_rgba(236,72,153,0.3)]
             animate-[pulse_2s_ease-in-out_infinite]"
         >
-          Enter Feed
+          Enter <ArrowBigRight size={20}/>
         </button>
       </div>
     </div>
